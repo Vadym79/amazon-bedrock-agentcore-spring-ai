b99c99ea835bbad3945886fa01cafe6e503c5079
Vadym Kazulkin <v.kazulkin@gmail.com>
2026-03-29 21:26:22 +0200

    upgraded to Java 25, Spring Boot 4 and Spring AI 1.1.3

---
+++ spring-ai-1.1-agent-demo/src/main/java/dev/vkazulkin/agent/sdk/InvokeRuntimeAgent.java
@@ -0,0 +1,33 @@
+package dev.vkazulkin.agent.sdk;
+
+import java.nio.charset.StandardCharsets;
+
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.bedrockagentcore.BedrockAgentCoreClient;
+import software.amazon.awssdk.services.bedrockagentcore.model.InvokeAgentRuntimeRequest;
+
+
+public class InvokeRuntimeAgent {
+
+	private static final String AGENT_RUNTIME_ARN="{AGENTCORE_RUNTIME_ARN}";
+	
+	public static void main(String[] args) throws Exception {
+
+		String payload = "{\"prompt\":\"Give me an overview of the order with the id equals 100\"}";
+		BedrockAgentCoreClient bedrockAgentCoreClient = BedrockAgentCoreClient.builder().region(Region.US_EAST_1)
+				.build();
+
+		var invokeAgentRuntimeRequest = InvokeAgentRuntimeRequest.builder()
+				.agentRuntimeArn(AGENT_RUNTIME_ARN)
+				.qualifier("DEFAULT").contentType("application/json").payload(SdkBytes.fromUtf8String(payload)).build();
+		try (var responseStream = bedrockAgentCoreClient
+				.invokeAgentRuntime(invokeAgentRuntimeRequest)) {
+			var text = new String(responseStream.readAllBytes(), StandardCharsets.UTF_8);
+
+			System.out.println(text);
+		}
+
+	}
+
+}
