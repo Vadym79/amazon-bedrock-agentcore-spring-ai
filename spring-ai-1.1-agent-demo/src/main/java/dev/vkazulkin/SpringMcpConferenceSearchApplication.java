8daf1ddc36ae23649dd8191896d32226d2752836
Vadym Kazulkin <v.kazulkin@gmail.com>
2026-03-30 08:14:18 +0200

    upgraded to Java 25, Spring Boot 4 and Spring AI 1.1.3

--- spring-ai-1.1-agent-demo/src/main/java/dev/vkazulkin/SpringMcpConferenceSearchApplication.java
+++ spring-ai-1.1-agent-demo/src/main/java/dev/vkazulkin/SpringMcpConferenceSearchApplication.java
@@ -2,6 +2,9 @@ package dev.vkazulkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class SpringMcpConferenceSearchApplication {
@@ -9,4 +12,9 @@ public class SpringMcpConferenceSearchApplication {
 	public static void main(String[] args) {
 			SpringApplication.run(SpringMcpConferenceSearchApplication.class, args);
 	}
+	
+	@Bean
+    public ObjectMapper objectMapper() {
+       return new ObjectMapper();
+    }
 }
