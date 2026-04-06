c1b11d8ec0c23c765a74fd958a9ce187727add70
Vadym Kazulkin <v.kazulkin@gmail.com>
2026-04-06 20:55:32 +0200

    iac with aws cdk for java for spring ai 1.1 conference search and application mcp servers on agentcore using runtime and gateway

---
+++ spring-ai-1.1-conference-search-and-application-bedrock-agentcore-mcp-server-cdk/README.md
@@ -0,0 +1,24 @@
+# IaC
+
+AWS CDK for Java infrastructure for Conference Search And Application with Spring AI deployed on different Amazon Bedrock Agentcore MCP servers based on AgentCore Runtime and Agentcore Gateway 
+
+## Configure
+
+configure the properties in cdk.json which march our environment
+
+## Deploy
+
+```bash
+# all stacks
+mvn clean package && cdk deploy -c awsAccountId={YOUR_AWS_ACCOUINT_ID} -c restApiId={YOUR_AMAZON_GATEWAY_REST_API_ID} --all
+
+# available stacks names are: 
+spring-ai-conference-search-agentcore-user-client-pool-stack
+spring-ai-conference-search-agentcore-runtime-with-mcp-server-stack 
+spring-ai-conference-search-agentcore-gateway-with-mcp-server-target-stack
+
+mvn clean package && cdk deploy {STACK_NAME} -c awsAccountId={YOUR_AWS_ACCOUINT_ID} -c restApiId={YOUR_AMAZON_GATEWAY_REST_API_ID}
+ 
+```
+
+
---
+++ spring-ai-1.1-conference-search-and-application-bedrock-agentcore-mcp-server-cdk/src/main/java/dev/vkazulkin/agentcore/runtime/RuntimeWithMCPStack.java
@@ -0,0 +1,49 @@
+package dev.vkazulkin.agentcore.runtime;
+
+
+import java.util.List;
+
+import dev.vkazulkin.ConventionalDefaults;
+import dev.vkazulkin.cognito.UserClientPoolStack;
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.bedrock.agentcore.alpha.AgentRuntimeArtifact;
+import software.amazon.awscdk.services.bedrock.agentcore.alpha.ProtocolType;
+import software.amazon.awscdk.services.bedrock.agentcore.alpha.Runtime;
+import software.amazon.awscdk.services.bedrock.agentcore.alpha.RuntimeAuthorizerConfiguration;
+import software.amazon.awscdk.services.iam.IRole;
+import software.amazon.awscdk.services.iam.Role;
+import software.constructs.Construct;
+
+public class RuntimeWithMCPStack extends Stack {
+	
+	public static IRole role;
+	
+	public static Runtime runtime;
+
+    public RuntimeWithMCPStack(Construct scope, String appName,  StackProps stackProps) {
+    	var id=ConventionalDefaults.stackName(appName, "runtime-with-mcp-server");
+        super(scope, id, stackProps);   
+        System.out.println(" stack id "+id);
+        
+        var ecrImageURI=ConventionalDefaults.getContextVariableValueWithReplacedAccountId(this, "ecrImageURIForConferenceSearchAndApplicationAppAsMCPServer");     		
+        var roleArnForTheAgentCoreRuntime=ConventionalDefaults.getContextVariableValueWithReplacedAccountId(this, "roleArnForTheAgentCoreRuntime");
+       
+        // The runtime by default create ECR permission only for the repository available in the account the stack is being deployed
+        var agentRuntimeArtifact = AgentRuntimeArtifact.fromImageUri(ecrImageURI);
+        role= Role.fromRoleArn(this,"roleArnForTheAgentCoreRuntimeRole", roleArnForTheAgentCoreRuntime);
+     
+        // Create runtime using the built image
+        runtime = Runtime.Builder.create(this, "MCPRuntime-123")
+                .runtimeName(appName.replace("-", "_")+ "_runtime")
+                .authorizerConfiguration(RuntimeAuthorizerConfiguration.usingJWT(UserClientPoolStack.COGNITO_DISCOVERY_URL, List.of(UserClientPoolStack.userPoolClient.getUserPoolClientId()), null))
+                .description("AgenCore Runtime with MCP protocol for running conference search app")
+                .protocolConfiguration(ProtocolType.MCP)
+                .agentRuntimeArtifact(agentRuntimeArtifact)
+                .executionRole(role)
+                .build();
+        
+        CfnOutput.Builder.create(this, "RuntimeIdOutput").value(runtime.getAgentRuntimeId()).build();           
+     }  
+}
