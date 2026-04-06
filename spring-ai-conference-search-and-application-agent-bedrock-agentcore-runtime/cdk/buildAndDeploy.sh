a5baff3f7dcc493c33517f2a02d5d395f642cdec
Vadym Kazulkin <v.kazulkin@gmail.com>
2026-04-06 20:56:26 +0200

    Delete spring-ai-conference-search-and-application-agent-bedrock-agentcore-runtime directory

--- spring-ai-conference-search-and-application-agent-bedrock-agentcore-runtime/cdk/buildAndDeploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -e
-mvn clean package && cdk deploy -c awsAccountId={YOUR_AWS_ACCOUINT_ID} --all
--- spring-ai-conference-search-and-application-agent-bedrock-agentcore-runtime/cdk/buildAndDeployDontAsk.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -e
-mvn clean package && cdk deploy --all --require-approval=never -c awsAccountId={YOUR_AWS_ACCOUINT_ID} --all
