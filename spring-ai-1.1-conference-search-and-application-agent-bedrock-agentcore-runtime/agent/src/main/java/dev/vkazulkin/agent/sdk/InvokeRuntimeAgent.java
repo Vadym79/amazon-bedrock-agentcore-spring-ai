447573d9f0280419ae9c41582bfcb8e71c82b189
Vadym Kazulkin <v.kazulkin@gmail.com>
2026-04-06 20:49:46 +0200

    spring ai 1.1 conference search and application agent on amazon bedrock agentcore runtime including iac with cdk for java

---
+++ spring-ai-1.1-conference-search-and-application-agent-bedrock-agentcore-runtime/agent/src/main/java/dev/vkazulkin/agent/sdk/InvokeRuntimeAgent.java
@@ -0,0 +1,45 @@
+package dev.vkazulkin.agent.sdk;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.bedrockagentcore.BedrockAgentCoreClient;
+import software.amazon.awssdk.services.bedrockagentcore.model.InvokeAgentRuntimeRequest;
+
+
+public class InvokeRuntimeAgent {
+
+	private static final String AGENT_RUNTIME_ARN="{YOUR_RUNTIME_ARN}";
+	
+	public static void main(String[] args) throws Exception {
+
+		String payload =
+			"{\"prompt\":\"Please provide me with the list of the conferences including their ids with Java topic hapenning in 2027 with call for papers open today. Also provide me with the list of my talks with this topic in the title. Finally, for each conference and talk retrieved, apply individually for the conference.\"}";
+		
+		 //String payload =
+		 //"{\"prompt\":\"Please provide me with the list of the conferences including their ids with Java topic hapenning in 2027 with call for papers open today. Also provide me with the list of my talks with this topic in the title.\"}";
+	
+		var bedrockAgentCoreClient = BedrockAgentCoreClient.builder().overrideConfiguration(ClientOverrideConfiguration.builder()
+		        .apiCallTimeout(Duration.ofMinutes(3))
+		        .apiCallAttemptTimeout(Duration.ofMinutes(3))
+		        .build())
+				
+				.region(Region.US_EAST_1)
+				.build();
+
+		var invokeAgentRuntimeRequest = InvokeAgentRuntimeRequest.builder()
+				.agentRuntimeArn(AGENT_RUNTIME_ARN)				 
+				.qualifier("DEFAULT").contentType("application/json").payload(SdkBytes.fromUtf8String(payload)).build();
+		try (var responseStream = bedrockAgentCoreClient
+				.invokeAgentRuntime(invokeAgentRuntimeRequest)) {
+			var text = new String(responseStream.readAllBytes(), StandardCharsets.UTF_8);
+
+			System.out.println(text);
+		}
+
+	}
+
+}
---
+++ spring-ai-1.1-conference-search-and-application-agent-bedrock-agentcore-runtime/cdk/README.md
@@ -0,0 +1,23 @@
+# IaC
+
+AWS CDK for Java infrastructure for Conference Search And Application Agent implemented with Spring AI deployed on Amazon Bedrock Agentcore MCP servers based on AgentCore Runtime
+This agent acts as an MCP Client to talk to the tool to search for conferences and talk and apply to the conferences. Those tools are deploy on AgentCore Runrime and Gateway. 
+
+## Configure
+
+configure the properties in cdk.json which march our environment
+
+## Deploy
+
+```bash
+# all stacks
+mvn clean package && cdk deploy -c awsAccountId={YOUR_AWS_ACCOUINT_ID}  --all
+
+# available stacks names are: 
+spring-ai-conference-search-agentcore-runtime-with-mcp-server-stack 
+
+mvn clean package && cdk deploy {STACK_NAME} -c awsAccountId={YOUR_AWS_ACCOUINT_ID}
+ 
+```
+
+
