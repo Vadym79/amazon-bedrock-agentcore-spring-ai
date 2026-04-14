1a6f6456f5adeb1c2ddec52bc2f2457ff8215bad
Vadym Kazulkin <v.kazulkin@gmail.com>
2026-04-14 09:05:48 +0200

    some renamings

--- spring-ai-1.1-conference-app-agent-local/src/main/java/dev/vkazulkin/SpringAIConferenceLocalMCPClient.java
+++ spring-ai-1.1-conference-app-agent-local/src/main/java/dev/vkazulkin/SpringAIConferenceLocalMCPClient.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAIConferenceSearchAndApplicationOnAgentCore {
+public class SpringAIConferenceLocalMCPClient {
 
 	public static void main(String[] args) {
-			SpringApplication.run(SpringAIConferenceSearchAndApplicationOnAgentCore.class, args);
+			SpringApplication.run(SpringAIConferenceLocalMCPClient.class, args);
 	}
 }
